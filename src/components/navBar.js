059931ac0e739ed5b5596537e3fd2f0b734ddb03
lachagus <chagusbigand@gmail.com>
2022-08-23 20:53:13 -0300

    NavBar

---
+++ src/components/navBar.js
@@ -0,0 +1,28 @@
+const NavBar = () => {
+    return (
+        <header className="header">
+
+            <nav className="menu">
+                <ul className="items">
+                    <li>
+                        <a className="home" href="/#">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="/#">Papelería</a>
+                    </li>
+                    <li>
+                        <a href="/#">Vinilos</a>
+                    </li>
+                    <li>
+                        <a href="/#">Sublimación</a>
+                    </li>
+                </ul>
+            </nav>
+
+        </header>
+    )
+}
+
+export default NavBar;
+
+
